ed9fb4ef19872432a28eb0d31e94230cd20c9c91
Alex Heinen & Fernanda M M Getschko <pair+aheinen.fmgetschko@devbootcamp.com>
2015-06-23 11:39:35 -0700

    change options to match all routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
   get '/users/:user_id/wishlist' => 'wishlists#index'
   get '/users/:user_id/events/list' => 'events#list'
-  match '/users/:id' => 'users#render_204', via: [:options]
+  match '*' => 'users#render_204', via: [:options]
   # Access All Categories
   # get '/categories' => 'users#categories'
 
