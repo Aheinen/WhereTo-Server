81a22a49b04009a42294a82118ef68db699ce3d0
Alex Heinen <alexheinen99@gmail.com>
2015-06-25 18:59:36 -0700

    change migration on start date

--- db/migrate/20150619180247_create_events.rb
+++ db/migrate/20150619180247_create_events.rb
@@ -8,7 +8,7 @@ class CreateEvents < ActiveRecord::Migration
       t.string :price
       t.string :image
       t.boolean :free
-      t.string :start_time
+      t.datetime :start_time
       t.datetime :end_time
       t.string :performer
 
