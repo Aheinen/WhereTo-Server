81945efe3ea07b0c4d3bf72620e80d1b4a0e2937
Alex Heinen <alexheinen99@gmail.com>
2015-06-25 19:21:51 -0700

    fix migration

--- db/migrate/20150626021153_change_event_db.rb
+++ db/migrate/20150626021153_change_event_db.rb
@@ -1,5 +1,5 @@
 class ChangeEventDb < ActiveRecord::Migration
   def change
-    change_column(:events, :start_time, :datetime)
+    change_column :events, :start_time, 'datetime USING CAST(start_time AS datetime)'
   end
 end
